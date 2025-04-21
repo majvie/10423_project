51a0f36d049ff4c9ebc00f378ac182fecaeaae81
Maximilian Vieweg <maximilian.vieweg@gmail.com>
2025-04-20 21:51:54 -0400

    max fixed bug and regenerated data

--- biasing.py
+++ biasing.py
@@ -1,15 +1,20 @@
 import os
 import openai
+import pandas as pd
+import numpy as np
+from pprint import pprint
 from dotenv import load_dotenv
 
 from sentence_transformers import SentenceTransformer
 from sentence_transformers import util
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 load_dotenv()
 
+TITLE = "title.y"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
+
 
 class AI_Summarizer:
     def __init__(self, reviews, 
@@ -29,12 +34,14 @@ class AI_Summarizer:
         self.topk = topk
         self.max_context_length = 4096
 
+        self.reviews_filtered = None
+        self.reviews_method = None
+
     def create_context(self, reviews, biased=False):
         context = []
         for i, review in reviews.iloc[:self.topk].iterrows():
             review_text = f"Title: {review[TITLE]}\nReview: {review['text']}\n\n"
-            if len(context) + len(review_text) > self.max_context_length:
-                break
+            review_text = review_text[:self.max_context_length]
             context.append([review_text, review['rating']])
 
         if biased:
@@ -75,7 +82,7 @@ class AI_Summarizer:
         query_embedding = self.similarity_model.encode(query)
 
         similarities = []
-        for index, row in tqdm(reviews.iterrows(), total=len(reviews)):
+        for index, row in tqdm(reviews.iterrows(), total=len(reviews), disable=False):
             text_to_embed = f"{row[TITLE]} {row['text']}"
             embeddings = self.similarity_model.encode(text_to_embed)
 
@@ -98,6 +105,7 @@ class AI_Summarizer:
         - "filter": Ranking bias: The star rating is used to rank the reviews.
         - "ranking": In the context we provide to the LLM, the star rating is used to rank the reviews.
         - "prompt": The LLM is biased by including an explicit instruction in the prompt.
+        - "none": No bias is applied.
         """
 
         # TODO: to make it faster we should not recalculate reviews for each 
@@ -133,3 +141,58 @@ class AI_Summarizer:
             # "reviews": reviews_sorted, # Keep for debug
             "prompt": prompt,}
         return answer_obj
+
+query = "image quality"
+bias_types = {
+    "filter": [],
+    "none": [],
+    "ranking": [], 
+    "prompt": [],
+}
+
+
+if __name__ ==  "__main__":
+    # the meta csv joins meta data with the reviews
+    df = pd.read_csv('electronics_reviews_with_meta.csv')
+    df = df[df['training'] == 1]
+
+    column_names = ['parent_asin', 'title', 'average_rating', 'bias_type', 'text', 'sentiment_score', 'agreement_score', 'similarity_score']
+    df_data = pd.DataFrame(columns=column_names)
+
+    unique_product_names = df['parent_asin'].unique().tolist()
+
+    number_of_products = 100
+    for product_code in tqdm(unique_product_names[:number_of_products]):
+        matching_reviews = df[df['parent_asin'] == product_code]
+        model = AI_Summarizer(matching_reviews)
+
+        # Display the results
+        for bias_type, answer_obj in bias_types.items():
+            print(f"Bias Type: {bias_type}")
+            answer_obj = model.get_summary(query, bias_type=bias_type)
+            print(f"Bias Type: {answer_obj['bias_type']}")
+            print(f"Query: {answer_obj['query']}")
+            pprint(f"Answer: {answer_obj['answer']}")
+            print("\n")
+
+            # ['parent_asin', 'title', 'average_rating', 'bias_type', 'text', 'sentiment_score', 'agreement_score', 'similarity_score']
+
+            average_rating = float(matching_reviews['average_rating'].iloc[0])
+            row = [
+                product_code,
+                matching_reviews.iloc[0][TITLE],
+                average_rating,
+                bias_type,
+                answer_obj['answer'],
+                None, 
+                None, 
+                None,
+            ]
+            df_data = pd.concat([pd.DataFrame([row], columns=df_data.columns), df_data], ignore_index=True)
+
+            # save the dataframe during iterations 
+            df_data.to_csv('bias_results_.csv', index=False)
+
+    # save the dataframe finally
+    df_data.to_csv('bias_results.csv', index=False)
+    print("Dataframe saved to bias_results.csv")
